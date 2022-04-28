a37246f7643d3acca79f31c5214125562138f595
erdinc-agbulut <erdinc.agbulut@gmail.com>
2022-04-28 08:52:11 +0200

    Ajouts des DAO

--- src/main/java/edu/mns/locmns/LocMnsApplication.java
+++ src/main/java/edu/mns/locmns/LocMnsApplication.java
@@ -2,9 +2,19 @@ package edu.mns.locmns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class LocMnsApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(LocMnsApplication.class, args);
---
+++ src/main/java/edu/mns/locmns/dao/UtilisateurDao.java
@@ -0,0 +1,15 @@
+package edu.mns.locmns.dao;
+
+import edu.mns.locmns.model.Utilisateur;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UtilisateurDao extends JpaRepository<Utilisateur, Integer> {
+
+    Optional<Utilisateur> findByMail(String mail);
+
+
+}
